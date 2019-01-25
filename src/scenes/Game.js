4c7e044f3d79e8b5296e136fd9c8289e4d041c4a
Mali <smallbro@live.co.uk>
2019-01-25 19:10:00 +0000

    Added basic tiled map loading

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,11 +5,16 @@ export default class Game extends Phaser.Scene {
     super('Game');
   }
 
-  preload() {
-
-  }
-  
   create() {
+    const camera = this.cameras.main;
+
+ 	  const map = this.make.tilemap({key: 'map'});
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    
+    const tileset = map.addTilesetImage("testtiles", "tiles");
+
+    const belowLayer = map.createStaticLayer("test", tileset, 0, 0);
+   
     this.add.text(10, 10, 'Welcome to the game', { font: '48px Arial', fill: '#fff' });
   }
 }
