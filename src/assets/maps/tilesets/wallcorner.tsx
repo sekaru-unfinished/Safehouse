dbce82b5c19182f69c2c943dbb9f5ce58ad8e22e
Tudor <sekaru@sleepystudios.net>
2019-01-26 15:22:29 +0000

    Map tilesets

---
+++ src/assets/maps/tilesets/wallcorner.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="wallcorner" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="../wallcorner.png" width="32" height="32"/>
+</tileset>
--- src/maps/MapManager.js
+++ src/maps/MapManager.js
@@ -1,40 +1,40 @@
 export default class {
 
-	constructor(level = 1, scene){
-		this.level = level;
-		this.scene = scene;
-		this.loadLevel();
-	}
+  constructor(level = 1, scene) {
+    this.level = level;
+    this.scene = scene;
+    this.loadLevel();
+  }
 
-	getCameraBounds(){
-		return {
-			width: this.map.widthInPixels,
-			height: this.map.heightInPixels
-		}
-	}
+  getCameraBounds() {
+    return {
+      width: this.map.widthInPixels,
+      height: this.map.heightInPixels
+    }
+  }
 
-	loadLevel(){
-		const map = this.scene.make.tilemap({key: `level_${this.level}`});
-    
-    	const tileset = map.addTilesetImage("testtiles", "tiles");
-    	const baseLayer = map.createDynamicLayer("floor", tileset, 0, 0);
-    	const collisionsLayer = map.createDynamicLayer("collisions", tileset, 0, 0);
-		collisionsLayer.setCollisionByProperty({collides: true});
-		collisionsLayer.setCollisionFromCollisionGroup();
-	    this.scene.matter.world.convertTilemapLayer(collisionsLayer);	
+  loadLevel() {
+    const map = this.scene.make.tilemap({ key: `level_${this.level}` });
 
-		this.scene.matter.world.createDebugGraphic();
-		this.map = map;
+    const tileset = map.addTilesetImage("testtiles", "tiles");
+    const baseLayer = map.createDynamicLayer("floor", tileset, 0, 0);
+    const collisionsLayer = map.createDynamicLayer("Walls", tileset, 0, 0);
+    collisionsLayer.setCollisionByProperty({ collides: true });
+    collisionsLayer.setCollisionFromCollisionGroup();
+    this.scene.matter.world.convertTilemapLayer(collisionsLayer);
 
-		const cameraWidth = this.getCameraBounds().width;
-		const cameraHeight = this.getCameraBounds().height;
+    this.scene.matter.world.createDebugGraphic();
+    this.map = map;
 
-		this.scene.matter.world.setBounds(0, 0, cameraWidth, cameraHeight);
-		console.log(this.scene)
-	}
+    const cameraWidth = this.getCameraBounds().width;
+    const cameraHeight = this.getCameraBounds().height;
 
-	loadNextLevel(){
-		this.level++;
-		this.loadLevel();
-	}
+    this.scene.matter.world.setBounds(0, 0, cameraWidth, cameraHeight);
+    console.log(this.scene)
+  }
+
+  loadNextLevel() {
+    this.level++;
+    this.loadLevel();
+  }
 }
