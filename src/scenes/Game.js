445b879091fd1f24a169820c0c41d012a5b162f9
Tudor <sekaru@sleepystudios.net>
2019-01-26 10:03:50 +0000

    Speaker + clicking it, intellisense config for vscode

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,7 +3,6 @@ import MapManager from "../maps/MapManager";
 import Player from "../gameplay/Player";
 
 export default class Game extends Phaser.Scene {
-
     constructor() {
         super('Game');
 
@@ -15,6 +14,8 @@ export default class Game extends Phaser.Scene {
     preload() {
       this.load.image('player', 'src/assets/sprites/fridge.png');
       this.load.image('phone', 'src/assets/images/phone.png');
+      this.load.image('speaker_off', 'src/assets/images/icons/speaker_off.png');
+      this.load.image('speaker_on', 'src/assets/images/icons/speaker_on.png');
     }
 
     create() {
@@ -35,11 +36,14 @@ export default class Game extends Phaser.Scene {
         });
 
         this.phone = this.add.image(0, 0, 'phone');
-        const text = this.add.text(0, 0, 'iPhones are bad', { font: '12px Arial', fill: '#000' });
-        text.x -= text.width / 2;
+        this.phone.setScale(0.75);
+        const speaker = this.add.image(0, 0, 'speaker_off').setInteractive();
+        speaker.setScale(1.25);
+        
+        this.input.on('gameobjectdown', this.onPhoneClick);
 
         this.phoneContainer = this.add.container(this.phone.width / 2 + 10, this.sys.game.canvas.height + this.phone.height) 
-        this.phoneContainer.add([this.phone, text]);
+        this.phoneContainer.add([this.phone, speaker]);
 
         this.input.keyboard.on('keydown_SPACE', () => {
           this.showPhone = !this.showPhone;
@@ -50,6 +54,10 @@ export default class Game extends Phaser.Scene {
             ease: 'Power2'
           });
       }, this);
+    }
+
+    onPhoneClick(pointer, gameObject) {
+        gameObject.setTexture('speaker_on');
     }
 
     update() {
