179fe0001c25e0b171bf365bb586479f1f271db4
Tudor <sekaru@sleepystudios.net>
2019-01-27 02:59:48 +0000

    Polish on room toggling

--- src/maps/Room.js
+++ src/maps/Room.js
@@ -1,32 +1,32 @@
-export default class{
-	
-	constructor(scene, x, y, width, height, id, graphics){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.id = id;
-		this.scene = scene;
+export default class {
 
-		this.graphics = graphics;
-     	this.overlay = new Phaser.Geom.Rectangle(this.x, this.y, this.width, this.height);
-	}
+    constructor(scene, x, y, width, height, id, graphics) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.id = id;
+        this.scene = scene;
 
-	shouldRoomRevealForPlayer(player){
-		if(Phaser.Geom.Rectangle.Overlaps(player.getBounds(), this.overlay)){
-			this.showRoom = false;
-		}else{
-			this.showRoom = true;
-		}
-	}
+        this.graphics = graphics;
+        this.graphics.setDepth(4);
+        this.overlay = new Phaser.Geom.Rectangle(this.x, this.y, this.width, this.height);
+        this.shownAlpha = 1;
+    }
 
-	render(){
-		this.graphics.fillStyle(0x0f0c27,
-			this.showRoom ? 1 : 0);
+    shouldRoomRevealForPlayer(player) {
+        if (Phaser.Geom.Rectangle.Overlaps(player.getBounds(), this.overlay)) {
+            this.showRoom = false;
+        } else {
+            this.showRoom = true;
+        }
+    }
+
+    render() {
+        const target = this.showRoom ? 1 : 0;
+        this.shownAlpha += (target - this.shownAlpha) * 0.2;
+        this.graphics.fillStyle(0x26243c, this.shownAlpha);
 
         this.graphics.fillRectShape(this.overlay);
-
-        this.graphics.fillStyle(0x0f0c27,
-        	!this.showRoom ? 0 : 1);
-	}
+    }
 }
