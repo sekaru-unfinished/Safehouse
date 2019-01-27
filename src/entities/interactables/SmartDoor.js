a45c14a4b5ac575794cc15230f8285c1e152419b
Mali <smallbro@live.co.uk>
2019-01-27 06:38:15 +0000

    Added smartdoor

---
+++ src/entities/interactables/SmartDoor.js
@@ -0,0 +1,23 @@
+import Interactable from '../Interactable';
+
+export default class extends Interactable{
+	
+	constructor(scene, x, y, direction){
+		super({scene, x, y, key: 'smart_door'});
+		this.setStatic(true);
+		this.triggered();
+	}
+
+	triggered(){
+		this.x -= 32;
+     //    this.scene.tweens.add({
+	    //     targets: bullet,
+	    //     x: this.x,
+	    //     y: this.y,
+	    //     duration: 150,
+	    //     onComplete: this.destroyBullet,
+	    //     onCompleteParams: [this]
+	    // });
+	}
+
+}
