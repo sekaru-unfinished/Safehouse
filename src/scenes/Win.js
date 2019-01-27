028d71c8413473a205def7641fb21683c212d131
tomekp97 <tomek.pryjma@gmail.com>
2019-01-27 04:43:45 +0000

    Amend text on WinScene

--- src/scenes/Win.js
+++ src/scenes/Win.js
@@ -6,8 +6,10 @@ export default class WinScene extends Phaser.Scene {
     }
 
     create() {
+        const sceneContext = this.scene;
         const width = this.sys.canvas.width;
         const height = this.sys.canvas.height;
+        let textXPosition = width / 2;
         
         let graphics = this.add.graphics();
 
@@ -34,5 +36,32 @@ export default class WinScene extends Phaser.Scene {
             loop: 0,
             yoyo: false
         })
+
+        this.restartText = this.add.text(textXPosition, height + 100, "Press any key to restart", {
+            font: '30px monospace',
+            fill: '#ffffff'
+        });
+        this.restartText.setOrigin(0.5);
+
+        this.tween = this.tweens.add({
+            targets: this.restartText,
+            y: height - 100,
+            duration: 1500,
+            completeDelay: 800,
+            ease: 'Power2',
+            delay: 500,
+            repeat: 0,
+            loop: 0,
+            yoyo: false,
+            onComplete: this.restartGame,
+            callbackScope: sceneContext
+        });
+    }
+
+    restartGame(sceneContext) {
+        this.scene.input.keyboard.on('keydown', function(event) {
+            this.scene.sound.add('tap').play();
+            this.start('Menu');
+        }, this)
     }
 }
