382073222fae5f8bc08957fb336084f445421b62
tomekp97 <tomek.pryjma@gmail.com>
2019-01-27 03:57:23 +0000

    Amend text on the Menu scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -24,21 +24,30 @@ export default class Menu extends Phaser.Scene {
     const width = this.sys.canvas.width;
     const height = this.sys.canvas.height;
     
+    let textXPosition = width / 2;
     let graphics = this.add.graphics();
 
     graphics.fillStyle(0x2f4f4f, 1);
     graphics.fillRect(0, 0, width, height);
 
-    this.text = this.add.text(width / 2, 50, "Name of the game", {
+    this.text = this.add.text(textXPosition, 150, "Name of the game", {
         font: '50px Courier',
         fill: '#ffffff'
     });
-    this.textPlay = this.add.text(width / 2, height - 200, "Press any key to play", {
+
+    let tutText = "Controls:\n---------\nUse the arrow keys to move around.\nPress space to toggle your phone and control your home.\n\nObjective:\n----------\nTrap, incapacitate or injure the intruders to complete each level.";
+    this.tutorial = this.add.text(textXPosition, 400, tutText, {
+      font: '25px monospace',
+      fill: '#ffffff'
+    })
+
+    this.textPlay = this.add.text(textXPosition, height - 50, "Press any key to play", {
         font: '30px monospace',
         fill: '#ffffff'
     });
 
     this.text.setOrigin(0.5);
+    this.tutorial.setOrigin(0.5);
     this.textPlay.setOrigin(0.5);
 
     this.cameras.main.fadeIn(1000);
